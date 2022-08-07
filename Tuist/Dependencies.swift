8eff1e391377e3c1589a20fdaa867f10fcf0f046
Yasin Akbas <yasin.kbas12@gmail.com>
2022-08-07 19:48:47 +0300

    remove tuist requirement

--- Tuist/Dependencies.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-
-let dependencies = Dependencies(
-    swiftPackageManager: [
-        .remote(url: "https://github.com/yasinkbas/UILab.git", requirement: .upToNextMajor(from: "0.3.3")),
-    ],
-    platforms: [.iOS]
-)
-
