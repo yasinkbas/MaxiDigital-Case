db6ae039028c26e32717be80161162da8a043ae6
Yasin Akbas <yasin.kbas12@gmail.com>
2022-08-07 19:28:00 +0300

    add gitignore

--- Tuist/Dependencies/SwiftPackageManager/Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version: 5.7
-import PackageDescription
-
-let package = Package(
-    name: "PackageName",
-    dependencies: [
-        .package(url: "https://github.com/yasinkbas/UILab.git", from: "0.3.3"),
-    ]
-)
